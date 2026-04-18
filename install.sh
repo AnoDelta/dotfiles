75dc011b34fb3063aee226f5d8b7f79411ca0e2d
AnoDelta <AnoDeltaz@gmail.com>
2026-04-19 01:38:49 +0800

    add gtk

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 cd "$(dirname "$0")"
 
 echo "Stowing dotfiles..."
-stow bash dunst emacs fastfetch git mimeapps nvim scripts vim xinitrc yazi
+stow bash dunst emacs fastfetch git mimeapps nvim scripts vim xinitrc yazi gtk
 
 echo "Stowing system files (requires sudo)..."
 sudo stow --target=/usr/local/bin system
